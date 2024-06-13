2792e6f88ef2446eeaf8447c54f1d99afe2a413d
Juyang_Jin <112486031+juyangjin@users.noreply.github.com>
2024-06-13 11:36:05 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.34 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        String[] list = my_string.trim().split("\\s+");
+        
+        return list;
+    }
+}
