146eda5adf7d965a3a1e0d78257af0e75acb0270
Juyang_Jin <112486031+juyangjin@users.noreply.github.com>
2024-06-18 11:14:49 +0900

    Delete 프로그래머스/lv0 directory

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++
@@ -1,7 +0,0 @@
-class Solution {
-    public double solution(double num1, double num2) {
-        double sum = num1 / num2;
-        double result = sum * 1000.0;
-        return (int)result;
-    }
-}
