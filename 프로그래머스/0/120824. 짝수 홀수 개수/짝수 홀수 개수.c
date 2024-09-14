df7888eedc52320ee70784d074cb6f49761bd874
Juyang_Jin <112486031+juyangjin@users.noreply.github.com>
2024-09-14 15:25:14 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 4.52 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.c
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.c
@@ -4,8 +4,7 @@
 
 int* solution(int num_list[], size_t num_list_len) {
     int* answer = (int*)malloc(2 * sizeof(int));
-    answer[0] = '\0';
-    answer[1] = '\0';
+    memset(answer, 0 ,sizeof(answer));
     
     for(int i=0;i<num_list_len;i++){
         if(num_list[i] % 2 == 0) answer[0]++;
