c7f128c3614bc18cca917508e8a1d28e6adf3b10
Juyang_Jin <112486031+juyangjin@users.noreply.github.com>
2024-09-21 10:30:20 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.01 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num_list[], size_t num_list_len) {
+    int a = 0;
+    int b = 0;
+    
+    for(int i=0;i<num_list_len;i++){
+        if(num_list[i] % 2) a = a * 10 + num_list[i];
+        else b = b * 10 + num_list[i];
+    }
+    
+    return a + b;
+}
