ad5c1a9cea3bf1f6a571143659dd03d15abb27f6
Juyang_Jin <112486031+juyangjin@users.noreply.github.com>
2024-09-28 10:16:53 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/README.md
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 정렬하기 (1) - 120850 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120850?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120850?language=c) 
 
 ### 성능 요약
 
-메모리: 76.4 MB, 시간: 0.40 ms
+메모리: 4.13 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 28일 09:58:59
+2024년 09월 28일 10:16:51
 
 ### 문제 설명
 
--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.c
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.c
@@ -2,9 +2,21 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int* compare(const int* a, const int* b){
+    return (*a - *b);
+}
+
 int* solution(const char* my_string) {
-    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    int* answer = (int*)malloc(1);
+    int* answer = (int*)malloc(strlen(my_string) * sizeof(int));
+    int len = 0;
+    
+    for(int i=0;i<strlen(my_string);i++){
+        if(isdigit(my_string[i])){
+            answer[len++] = my_string[i] - '0';
+        }
+    }
+    
+    qsort(answer, len, sizeof(int), compare);
+    
     return answer;
 }
