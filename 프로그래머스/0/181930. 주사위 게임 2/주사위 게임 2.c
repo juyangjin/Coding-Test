8431d54ecd545014d6a56ff2d2c1012eea19755b
Juyang_Jin <112486031+juyangjin@users.noreply.github.com>
2024-10-12 11:08:32 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 4.46 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.c
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.c
@@ -7,7 +7,6 @@ int solution(int a, int b, int c) {
     
     if(a == b && b == c) answer *= ((a * a) + (b * b) + (c * c)) *  ((a * a * a) + (b * b * b) + (c * c * c));
     else if(a == b || b == c || a == c) answer *= ((a * a) + (b * b) + (c * c));
-    else answer;
     
     return answer;
 }
