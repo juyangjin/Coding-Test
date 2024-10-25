46eaed3c098ccdaac66214b755c714f87997e184
Juyang_Jin <112486031+juyangjin@users.noreply.github.com>
2024-10-25 17:27:26 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/README.md
+++ 프로그래머스/1/12947. 하샤드 수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 하샤드 수 - 12947 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12947?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12947?language=java) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.04 ms
+메모리: 78.4 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 13일 10:28:30
+2024년 10월 25일 17:27:34
 
 ### 문제 설명
 
--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,18 +1,20 @@
 class Solution {
+    public boolean ha(int a, int b){
+        if(a % b == 0) return true;
+        return false;
+    }
+    
     public boolean solution(int x) {
         boolean answer = true;
-        String n = Integer.toString(x);
-        int[] num = new int[n.length()];
         int sum = 0;
+        int tmp = x;
         
-        for(int i=0;i<n.length();i++){ //배열에 값 추가하기 
-            num[i] = n.charAt(i)-48; //아스키코드값 빼기
+        while(tmp > 0){
+            sum += tmp % 10;
+            tmp /= 10;
         }
         
-        for(int tmp : num){//자바 배열값 더하기
-            sum += tmp;
-        } 
-        answer = x%sum == 0 ? true : false; //하샤드 수 구하기
+        answer = ha(x,sum);
         
         return answer;
     }
