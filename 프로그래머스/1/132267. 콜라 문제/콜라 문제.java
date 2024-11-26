0383bd79aac84f0ab79d04055be9780864f7170c
Juyang_Jin <wndid2008@gmail.com>
2024-11-26 12:37:03 +0900

    [level 1] Title: 콜라 문제, Time: 0.23 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -2,7 +2,6 @@ class Solution {
     public int coke(int a, int b, int n){
         return (n / a) * b;
     }
-    
     public int solution(int a, int b, int n) {
         int answer = 0;        
         while(n >= a){
