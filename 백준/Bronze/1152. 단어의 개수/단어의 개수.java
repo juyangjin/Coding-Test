4884f369dbc4bd147b299961bb9c6fcb2900b991
Juyang_Jin <wndid2008@gmail.com>
2025-10-23 11:14:57 +0900

    Update 단어의 개수.java

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -9,4 +9,22 @@ public class Main{
         int c = st.countTokens();
         System.out.print(c);
     }
+
 }
+
+// Scanner만 쓰인 버전 StringTokenizer는 빠르지만 레거시한 코드라서 주의.
+// import java.util.*;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         String s = sc.nextLine().trim(); // 앞뒤 공백 제거
+
+//         if (s.isEmpty()) { // 문자열이 공백뿐인 경우
+//             System.out.println(0);
+//         } else {
+//             String[] words = s.split(" "); // 공백 1개로만 구분
+//             System.out.println(words.length);
+//         }
+//     }
+// }
